dca6023978b8c3edabc23189d85acf27dfa28ce5
Venkatarajesh Jakka <venkatarajeshjakka@Venkatarajeshs-MacBook-Air.local>
2022-09-02 17:11:07 +0530

    added files

--- reactgraphql/src/App.js
+++ reactgraphql/src/App.js
@@ -1,11 +1,58 @@
+import github from "./db";
+import { useEffect, useState, useCallback } from 'react'
+import query from './Query'
 
 function App() {
+
+  let [userName, setUserName] = useState('');
+  let [repoList, setRepoList] = useState(null);
+
+  const fetchData = useCallback(() => {
+    fetch(github.baseURL, {
+      method: "POST",
+      headers: github.headers,
+      body: JSON.stringify(query)
+    }).then(response => response.json())
+      .then(data => {
+        const viewer = data.data.viewer;
+
+        setUserName(viewer.name);
+        setRepoList(viewer.repositories.nodes);
+
+        console.log(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+  }, []);
+
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
+
   return (
     <div className="App container mt-5">
-     <h1 className="text-primary">
-      <i className="bi bi-diagram-2-fill"></i>
-      Repos
-     </h1>
+      <h1 className="text-primary">
+        <i className="bi bi-diagram-2-fill"></i>
+        Repos
+      </h1>
+      <p>Hey there {userName}</p>
+      {
+        repoList && (
+          <ul className="list-group list-group-flush">
+            {
+              repoList.map((repo) => {
+                return (<li className="list-group-item" key={repo.id.toString()}>
+                  <a className="h5 mb-0 text-decoration-none" href={repo.url}>
+                    {repo.name}
+                  </a>
+                  <p className="small">{repo.description}</p>
+                </li>)
+              })
+            }
+          </ul>
+        )
+      }
     </div>
   );
 }
---
+++ reactgraphql/src/Query.js
@@ -0,0 +1,17 @@
+const githubQuery = {
+    query: `{
+      viewer{
+        name
+        repositories(first:10) {
+          nodes {
+            name
+            description
+            id
+            url
+          }
+        }
+      }
+    }`,
+  };
+
+export default githubQuery;
